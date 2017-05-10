b6f158ab495974ee845d5c403cad8d6c8ea767b3
Liam Winterton <liam.winterton@hotmail.co.uk>
2017-05-10 18:44:17 +0100

    Set up a beforeEach for initial setup

--- test/components/comment_box_test.js
+++ test/components/comment_box_test.js
@@ -2,20 +2,20 @@ import { renderComponent, expect } from '../test_helper'
 import CommentBox from '../../src/components/comment_box'
 
 describe('CommentBox', () => {
-    it('Has correct class', () => {
-        const component = renderComponent(CommentBox)
+    let component
 
+    beforeEach(() => {
+        component = renderComponent(CommentBox)
+    })
+
+    it('Has correct class', () => {
         expect(component).to.have.class('comment-box')
     })
     it('Has a text area', () => {
-        const component = renderComponent(CommentBox)
-
         expect(component.find('textarea')).to.exist
     })
 
     it('Has a button', () => {
-        const component = renderComponent(CommentBox)
-
         expect(component.find('button')).to.exist
     })
 })
