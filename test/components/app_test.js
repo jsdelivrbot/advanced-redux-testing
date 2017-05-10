5e5909cde3101237072357ecce016bdbe2c8f189
Liam Winterton <liam.winterton@hotmail.co.uk>
2017-05-10 18:53:38 +0100

    Create a test for rendering comment box

--- test/components/app_test.js
+++ test/components/app_test.js
@@ -1,15 +1,14 @@
 import { renderComponent, expect } from '../test_helper'
 import App from '../../src/components/app'
 
-// use describe to group together similar tests
 describe('app', () => {
-    // use 'it' to test a single attribute of a target
-    it('shows the correct text', () => {
+    let component
 
-        // create an instance of app
-        const component = renderComponent(App)
+    beforeEach(() => {
+        component = renderComponent(App)
+    })
 
-        // use 'expect' to make an assertion about a target
-        expect(component).to.contain('React simple starter')
+    it('CommentBox is rendered', () => {
+        expect(component.find('.comment-box')).to.exist
     })
 })
