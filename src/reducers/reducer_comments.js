a4b42c7ad3f02bdbc091a33f0250980b41659556
Liam Winterton <liam.winterton@hotmail.co.uk>
2017-05-11 16:22:12 +0100

    Handle undefined action in comments reducer

--- src/reducers/reducer_comments.js
+++ src/reducers/reducer_comments.js
@@ -1,6 +1,11 @@
 import { SAVE_COMMENT } from '../actions/types'
 
 export default function(state = [], action) {
+    switch(action) {
+        case undefined:
+            return state
+    }
+    
     switch(action.type) {
         case SAVE_COMMENT:
             return [ ...state, action.payload ]
