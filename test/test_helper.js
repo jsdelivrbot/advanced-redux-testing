9583af0ce97e85e9a3ff3eac2b171fb9c2998efa
Liam Winterton <liam.winterton@hotmail.co.uk>
2017-05-11 16:55:22 +0100

    Import jquery and use our fake DOM

--- test/test_helper.js
+++ test/test_helper.js
@@ -1,9 +1,11 @@
 import jsdom from 'jsdom'
+import jquery from 'jquery'
 
 // Set up a testing environment in to CML to run like a browser
 global.document = jsdom.jsdom('<!doctype html><html><body></body></html>')
 global.window = global.document.defaultView
 
+const $ = jquery(global.window)
 
 // Build 'renderComponent' helper to render a component
 
