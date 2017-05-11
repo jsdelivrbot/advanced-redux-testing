952cea0e9c20732baeeef15661b5f91eea78eb49
Liam Winterton <liam.winterton@hotmail.co.uk>
2017-05-11 16:51:36 +0100

    Start setting up jsdom

--- test/test_helper.js
+++ test/test_helper.js
@@ -1,4 +1,8 @@
+import jsdom from 'jsdom'
+
 // Set up a testing environment in to CML to run like a browser
+global.document = jsdom.jsdom('<!doctype html><html><body></body></html>')
+global.window = global.document.defaultView
 
 
 // Build 'renderComponent' helper to render a component
