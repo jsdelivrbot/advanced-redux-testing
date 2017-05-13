aa1a0d5204b73d00e1da2307d9d7c1377b6bbc36
Liam Winterton <liam.winterton@hotmail.co.uk>
2017-05-13 18:13:17 +0100

    Create the renderComponent function

--- test/test_helper.js
+++ test/test_helper.js
@@ -1,5 +1,6 @@
 import jsdom from 'jsdom'
 import jquery from 'jquery'
+import TestUtils from 'react-addons-test-utils'
 
 // Set up a testing environment in to CML to run like a browser
 global.document = jsdom.jsdom('<!doctype html><html><body></body></html>')
@@ -8,7 +9,11 @@ global.window = global.document.defaultView
 const $ = jquery(global.window)
 
 // Build 'renderComponent' helper to render a component
+function renderComponent(ComponentClass) {
+	const componentInstance = TestUtils.renderIntoDocument(<ComponentClass />)
 
+	return $(ReactDom.findDOMNode(componentInstance))
+}
 
 // Build a helper to simulate certain events in the browser/app
 
