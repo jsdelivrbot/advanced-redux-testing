b9a6bf90efd0eae1aca2bba0eac6ecbc11d75d0d
Liam Winterton <liam.winterton@hotmail.co.uk>
2017-05-16 12:41:16 +0100

    Provide a store to the component render and fix a ReactDOM typo

--- test/test_helper.js
+++ test/test_helper.js
@@ -1,8 +1,12 @@
 import React from 'react'
+import ReactDOM from 'react-dom'
 import jsdom from 'jsdom'
 import jquery from 'jquery'
 import TestUtils from 'react-addons-test-utils'
 import { expect } from 'chai'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import reducers from '../src/reducers'
 
 // Set up a testing environment in to CML to run like a browser
 global.document = jsdom.jsdom('<!doctype html><html><body></body></html>')
@@ -12,9 +16,13 @@ const $ = jquery(global.window)
 
 // Build 'renderComponent' helper to render a component
 function renderComponent(ComponentClass) {
-	const componentInstance = TestUtils.renderIntoDocument(<ComponentClass />)
+	const componentInstance = TestUtils.renderIntoDocument(
+		<Provider store={createStore(reducers)}>
+			<ComponentClass />
+		</Provider>
+	)
 
-	return $(ReactDom.findDOMNode(componentInstance))
+	return $(ReactDOM.findDOMNode(componentInstance))
 }
 
 // Build a helper to simulate certain events in the browser/app
